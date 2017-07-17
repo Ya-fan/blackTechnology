1b341b0570a8062255d0baefab02d5643fbcdec2
Ya-fan <38130740@qq.com>
2017-07-17 10:10:59 +0800

    Update params.php

--- config/params.php
+++ config/params.php
@@ -13,12 +13,7 @@ return [
    		'brand' => '/upload/brand',
    		'book' => '/upload/book',
    	],
-      'wx'=>[
-      'appid' => 'wx3dbcc87433ff8562',
-      'sk' => '82cb817129214cb982f42e17b03fa6d4',
-      'token' => 'weixin',
-      'aeskey' => '5DbU2smXd8wOlGSvpenesMDO7i5Mg7Fxi0f8Z7CRKDk',
-      ],
+      
       'title'=>[
             'name'=>'商城后台',
             'image'=>'/web/images/common/qrcode_for_gh_ca3d3993e573_430.jpg',
